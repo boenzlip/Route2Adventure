bb5d0f9109fa04122484762f7888bb0f9c6cf117
tgdbopa9 <patrick.boenzli@netcetera.ch>
2011-03-23 14:18:04 +0100

    setup project

---
+++ src/main/java/net/orxonox/gpr/App.java
@@ -0,0 +1,13 @@
+package net.orxonox.gpr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
